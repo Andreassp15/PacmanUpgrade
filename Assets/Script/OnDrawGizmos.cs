2a1f4aad969f52ea8e99694eeb661a4749387739
Axel Stenkrona <axelstenkrona@me.com>
2015-12-14 18:42:06 +0100

    Banor och Pause
    Jag har gjort colliders på de tre första banorna. Nu finns det ett pause
    scripts som fungerar. Trycker man på esc ska den dyka upp.

---
+++ Assets/Script/OnDrawGizmos.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OnDrawGizmos : MonoBehaviour {
+
+	void OnDrawGizmosYellow()
+	{
+		Gizmos.color = Color.yellow;
+		Gizmos.DrawCube(transform.position, new Vector3(1,1,1));
+	}
+}
