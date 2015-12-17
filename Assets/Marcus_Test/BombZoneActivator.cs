9c917fa810a8a527d367a363e878f91ed352ae3f
Heliga-Mufflan <Marcus.Overengen@hotmail.com>
2015-12-17 16:06:29 +0100

    Marcus, boss script
    Hej på er! =)

---
+++ Assets/Marcus_Test/BombZoneActivator.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+//*******//Main Zone Script//*******//
+
+public class BombZoneActivator : MonoBehaviour
+{
+
+
+	public GameObject bossActivated;
+		NewerBossEvent bossScript;
+
+
+	void Start ()
+	{
+		bossScript = bossActivated.GetComponent<NewerBossEvent> ();
+	}
+
+	public void OnTriggerEnter (Collider col)
+	{
+
+		if (col.gameObject.tag == "Pacman") 
+		{
+			bossScript.ZoneActivated (transform.position);
+			bossActivated.SetActive (true);
+			Debug.Log("Collided with the bomb zone");
+		}
+
+	}
+
+
+}
