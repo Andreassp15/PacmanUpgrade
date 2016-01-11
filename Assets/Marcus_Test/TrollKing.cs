826a70d75e924088c992466cff6dcd34c508f486
Heliga-Mufflan <Marcus.Overengen@hotmail.com>
2016-01-11 10:52:03 +0100

    Resterande banor

---
+++ Assets/Marcus_Test/TrollKing.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class TrollKing : MonoBehaviour {
+
+
+	void Start () 
+	{
+		gameObject.GetComponent<Animator> ().Play ("Idle");
+	}
+
+	public void PlayThrowAnimaton()
+	{
+		gameObject.GetComponent<Animator> ().Play ("Throw");
+		Invoke ("PlayIdleAnimation", 2.35f);
+	}
+
+	public void PlayIdleAnimation ()
+	{
+		gameObject.GetComponent<Animator> ().Play ("Idle");
+	}
+}
