81408cd2b68e27d67fc8095c86be184873ba0eda
Axel Stenkrona <axelstenkrona@me.com>
2016-01-12 11:53:12 +0100

    fixa val av karaktär
    Hade fel script på sig

---
+++ Assets/Script/Axels Scripts/MenuSystem/ReturnMainMenu.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class ReturnMainMenu : MonoBehaviour {
+
+	public void ReturnToMainMenu()
+	{
+		SceneManager.LoadScene("Startmeny");
+	}
+	public void Quit()
+	{
+		Application.Quit();
+	}
+}
